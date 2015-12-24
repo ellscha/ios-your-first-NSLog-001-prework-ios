31775772f84eeb87e625bf9c1f71ffb25db9b243
ellscha <escharlin@wesleyan.edu>
2015-12-24 15:15:27 -0500

    Update FISAppDelegate.m
    added changes in to the Github website because i dont know whats wrong.

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -9,7 +9,31 @@
     /**
     
      * Hey! Write your code here! :D
-     
+         NSLog(@"Dalek");
+    
+    NSLog(@"%@", @"Dalek");
+    
+    NSLog(@"%@%@%@%@%@", @"D", @"a", @"l", @"e", @"k");
+    
+    NSLog(@"Interpolate!");
+    
+    NSLog(@"%@", @"Interpolate!");
+    
+    NSLog(@"%@ %@", @"Interpolate!", @"Interpolate!");
+    
+    NSLog(@"%@%@%@%@!", @"In", @"ter", @"po", @"late");
+    
+    NSLog(@"%@-%@-%@-%@!", @"In", @"ter", @"po", @"late");
+    
+    NSLog(@"You are not The Doctor!");
+    
+    NSLog(@"%@", @"You are not The Doctor!");
+    
+    NSLog(@"%@%@%@%@%@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    
+    NSLog(@"%@ %@ %@ %@ %@%@", @"You", @"are", @"not", @"The", @"Doctor", @"!");
+    
+    NSLog(@"You are %@ %@!", @"not", @"The Dcotor");
      */
     
     // do not alter
