913a3ae2a238dc674f18f0e4c389dac14116f396
ellscha <Elli@Ellis-MacBook-Air.local>
2015-12-24 15:20:08 -0500

    there are changes

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -5,13 +5,14 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
+  
+    
     /**
     
      * Hey! Write your code here! :D
-     
-     */
-    
+            */
+    NSLog(@"Hello World!");
+
     // do not alter
     return YES;  //
 }   ///////////////
